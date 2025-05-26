69b8609b822a876df2e86bd33e41601503c1069c
ElizabeteLucindo <138694398+ElizabeteLucindo@users.noreply.github.com>
2025-05-26 17:45:26 +0000

    atualizado

--- schedule_rr_p.c
+++ schedule_rr_p.c
@@ -1,11 +1,62 @@
 #include "schedule_rr_p.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "task.h"
+#include "list.h"
+#include "CPU.h"
 
-// add a task to the list 
-void add(char *name, int priority, int burst){
-   int x = 0;
+#define QUANTUM 10
+
+struct node *priority_fila[MAX_PRIORITY + 1];
+int tid_counter = 0;
+
+void add(char *name, int priority, int burst) {
+    Task *t = malloc(sizeof(Task));
+    t->name = strdup(name);
+    t->priority = priority;
+    t->burst = burst;
+    t->deadline = 0;
+    t->tid = tid_counter++;
+
+    insert_end(&priority_fila[priority], t); 
 }
 
-// invoke the scheduler
-void schedule(){
-   int x = 0;
+
+void schedule() {
+    int tasks_restantes = 1;
+
+    while (tasks_restantes) {
+        tasks_restantes = 0;
+
+        for (int prioridade = MIN_PRIORITY; prioridade <= MAX_PRIORITY; prioridade++) {
+            if (priority_fila[prioridade] == NULL)
+                continue;
+
+            tasks_restantes = 1;
+
+            struct node *aux = priority_fila[prioridade];
+            Task *t = aux->task;
+
+            int exec_time;
+            if (t->burst > QUANTUM) {
+                  exec_time = QUANTUM;
+            } else {
+                  exec_time = t->burst;
+            }
+
+            run(t, exec_time);
+            t->burst -= exec_time;
+
+            delete(&priority_fila[prioridade], t);
+
+            if (t->burst > 0) {
+                insert_end(&priority_fila[prioridade], t);
+            } else {
+                free(t->name);
+                free(t);
+            }
+        }
+    }
 }
+
