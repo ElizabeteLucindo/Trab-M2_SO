19c801a80b3b7c0a8d6b1155629eb5b1827a3853
ElizabeteLucindo <138694398+ElizabeteLucindo@users.noreply.github.com>
2025-05-27 21:43:57 +0000

    atualizado

--- schedule_edf.c
+++ schedule_edf.c
@@ -26,13 +26,13 @@ void add_edf(char *name, int priority, int burst, int deadline) {
 }
 
 void schedule() {
-    start_timer();  // Inicia o timer em background
+    start_timer();  
 
     while (task_list != NULL) {
         struct node *aux = task_list;
         struct node *recente = task_list;
 
-        // Encontra a tarefa com deadline mais próximo
+        // Encontra deadline mais próximo
         while (aux != NULL) {
             if (aux->task->deadline < recente->task->deadline) {
                 recente = aux;
@@ -40,33 +40,28 @@ void schedule() {
             aux = aux->next;
         }
 
-        // Verifica se deadline já foi perdido antes de iniciar
+        // Verifica se deadline já foi perdido
         if (timer > recente->task->deadline) {
-            printf("Time %d: Missed deadline for task %s (Deadline: %d)\n",
+            printf("Time %d: Deadline perdido task %s Deadline: %d\n",
                    timer, recente->task->name, recente->task->deadline);
             delete(&task_list, recente->task);
             continue;
         }
 
-        int start_time = timer;
-
         printf("Time %d: Running task %s (Burst: %d, Deadline: %d)\n",
-               start_time, recente->task->name, recente->task->burst, recente->task->deadline);
+               timer, recente->task->name, recente->task->burst, recente->task->deadline);
 
-        // Executa a tarefa, 1 unidade de tempo por vez
+        
         for (int i = 0; i < recente->task->burst; ++i) {
-        while (!flag_estouro);   // espera o “tick”
+        while (!flag_estouro); // espera estouro
         flag_estouro = 0;
         run(recente->task, 1);
     }
 
-        int finish_time = timer;
-        int deadline_met = (finish_time <= recente->task->deadline);
-
-        if (deadline_met) {
-            printf("Time %d: Finished task %s (Met deadline)\n", finish_time, recente->task->name);
+        if (timer <= recente->task->deadline) {
+            printf("Time %d: Task finalizada %s \n", timer, recente->task->name);
         } else {
-            printf("Time %d: Finished task %s (Missed deadline)\n", finish_time, recente->task->name);
+            printf("Time %d: Task finalizada %s (Deadline perdido)\n", timer, recente->task->name);
         }
 
         delete(&task_list, recente->task);
